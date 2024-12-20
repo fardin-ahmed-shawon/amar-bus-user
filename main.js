61a4ac4ebce8f13b2e7256223f7616c2eb2aeddf
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2024-12-21 00:44:00 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Side Menubar
+const sidebar = document.getElementById("sidebar");
+
+function profile_open() {
+    sidebar.style.display = "block";
+}
+function profile_close() {
+    sidebar.style.display = "none";
+}
+
+
+
